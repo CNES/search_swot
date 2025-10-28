1513e932eb4c148410c6721d8795c8603105f164
Tonneau Anne-Sophie (CELAD) <atonneau@groupcls.com>
2025-10-28 15:38:01 +0100

    docs: models docstrings

--- search_swot/models.py
+++ search_swot/models.py
@@ -4,22 +4,27 @@ import pathlib
 
 
 class Mission(StrEnum):
+    """The mission selected in the application."""
     SWOT_SWATH = ('Swot - swath')
     SWOT_NADIR = ('Swot - nadir')
 
 
 class MissionType(Enum):
+    """The type of the mission: either nadir, or swath mission."""
     SWATH = auto()
     NADIR = auto()
 
 
 @dataclasses.dataclass
 class MissionProperties:
+    """Represents the properties of a mission."""
     mission_type: MissionType
     orf_file: str = dataclasses.field(default_factory=str)
     orbit_file: str = dataclasses.field(default_factory=str)
 
     def __post_init__(self):
+        """Checks that orf and orbit file exist, raises a FileNotFoundError if
+        not."""
         orf_file_path = pathlib.Path(__file__).parent / self.orf_file
         self.orf_file = orf_file_path.resolve(strict=True)
 
@@ -38,6 +43,18 @@ missions_properties = {
 
 
 class MissionPropertiesLoader:
+    """Utility class to load a mission's properties."""
 
-    def load(self, m: Mission):
+    def load(self, m: Mission) -> MissionProperties:
+        """Loads mission properties.
+
+        Parameters
+        ----------
+        m: Mission
+            a mission.
+
+        Returns
+        -------
+            the mission properties.
+        """
         return missions_properties[m]
